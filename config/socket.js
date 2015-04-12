086378bfca522a284ff1b4484f41f2fd75f89e2c
Emma Marriott <mekmarriott@gmail.com>
2015-04-12 03:48:36 -0700

    default to normal message paths for websockets and filter for speechtext

--- config/socket.js
+++ config/socket.js
@@ -69,7 +69,7 @@ module.exports = function(io, speechToText, alchemy) {
 
             if (sentiment && sentiment.score) {
               console.log("Sentiment score is " + sentiment.score);
-              socket.emit('sentiment', sentiment.score);
+              socket.emit('message', sentiment.score);
             }
 
           });
--- public/js/demo.js
+++ public/js/demo.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
   // UI
   var micButton = $('.micButton'),
     micText = $('.micText'),
-    transcript = $('#text'),
+    transcript = $('#speech-text'),
     errorMsg = $('.errorMsg');
 
   // Service
@@ -86,20 +86,22 @@ $(document).ready(function() {
 
   function showResult(data) {
     //if is a partial transcripts
-    if (data.results.length === 1 ) {
-      var paragraph = transcript.children().last(),
-        text = data.results[0].alternatives[0].transcript || '';
+    if (transcript){
+      if (data.results.length === 1 ) {
+        var paragraph = transcript.children().last(),
+          text = data.results[0].alternatives[0].transcript || '';
 
-      //Capitalize first word
-      text = text.charAt(0).toUpperCase() + text.substring(1);
-      // if final results, append a new paragraph
-      if (data.results[0].final){
-        text = text.trim() + '.';
-        $('<p></p>').appendTo(transcript);
+        //Capitalize first word
+        text = text.charAt(0).toUpperCase() + text.substring(1);
+        // if final results, append a new paragraph
+        if (data.results[0].final){
+          text = text.trim() + '.';
+          $('<p></p>').appendTo(transcript);
+        }
+        paragraph.text(text);
       }
-      paragraph.text(text);
+      transcript.show();
     }
-    transcript.show();
   }
 
   function displayError(error) {
